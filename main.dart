9b7bfdc445702a751fe66420189aae06913c6799
Md Shakil Ahamed <md.shakilahamedxox72@gmail.com>
2021-08-01 07:42:09 -0400

    compile time polymr

--- main.dart
+++ main.dart
@@ -1,45 +1,23 @@
 void main(List<String> args) {
-  Add add = Add();
-  var addresult = Calculator.calculate(2, 6, add);
-  print(addresult);
-
-  Sub sub = Sub();
-  var subresult = Calculator.calculate(2, 6, sub);
-  print(subresult);
-
-  Div div = Div();
-  var divresult = Calculator.calculate(2, 6, div);
-  print(divresult);
-
-  Mul mul = Mul();
-  var mulresult = Calculator.calculate(2, 6, mul);
-  print(mulresult);
+  Calculator.add(1, 2);
+  Calculator.add(1, 2, 2);
+  Man man = Man(5, 8);
+  Man women = Man(5, 8);
+  Man child = Man.fromXZ(5, 8);
 }
 
 abstract class Calculator {
-  static num calculate(num x, num y, Operation operation) {
-    if (operation is Add) {
-      return x + y;
-    }
-    if (operation is Sub) {
-      return x - y;
-    }
-    if (operation is Div) {
-      return x / y;
-    }
-    if (operation is Mul) {
-      return x * y;
-    }
-    return 0;
+  static add(int x, int y, [int z = 0]) {
+    print(x + y + z);
   }
 }
 
-abstract class Operation {}
+class Man {
+  int? x;
+  int? y;
+  int? z;
 
-class Add extends Operation {}
-
-class Sub extends Operation {}
-
-class Div extends Operation {}
-
-class Mul extends Operation {}
+  Man(this.x, this.y, [this.z = 10]);
+  Man.fromz(this.z);
+  Man.fromXZ(this.x, this.z);
+}
